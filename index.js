f18dde8e6ebfcbd75892ab251d588e6f05998f78
Rendal <23568313+sakhavhyand@users.noreply.github.com>
2023-12-22 22:57:51 +0100

    Adding/Removing Tags working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // An extension that allows you to manage tags.
 import { extension_settings } from '../../../extensions.js';
-import { callPopup, getEntitiesList, getThumbnailUrl, default_avatar } from '../../../../script.js';
-import { getTagsList } from '../../../tags.js';
+import { callPopup, getEntitiesList, getThumbnailUrl, setMenuType, setCharacterId, default_avatar, characters } from '../../../../script.js';
+import { getTagsList, createTagInput } from '../../../tags.js';
 
 const extensionName = 'SillyTavern-TagManager';
 const extensionFolderPath = `scripts/extensions/${extensionName}/`;
@@ -54,7 +54,7 @@ function getCharBlock(item, id) {
 function displayTag({ id, name, color }){
     let html = `<span id="${id}" class="tag" style="background-color: ${color};">
                     <span class="tag_name">${name}</span>
-                    <i class="fa-solid fa-circle-xmark tag_remove"></i>
+                    <i class="fa-solid fa-circle-xmark remove_tag"></i>
                 </span>`;
 
 
@@ -77,37 +77,32 @@ function fillDetails({ item, id, type }) {
                                     <div>${item.name}</div>
                                     <div>
                                         <div id="tagSearch">
-                                            <input id="tagInput" class="text_pole tag_input wide100p margin0 ui-autocomplete-input" data-i18n="[placeholder]Search / Create Tags" placeholder="Search / Create tags" maxlength="50" autocomplete="off">
+                                            <input id="input_tag" class="text_pole tag_input wide100p margin0 ui-autocomplete-input" data-i18n="[placeholder]Search / Create Tags" placeholder="Search / Create tags" maxlength="50" autocomplete="off">
                                         </div>
-                                        <div id="tagList" class="tags">
+                                        <div id="tag_List" class="tags">
                                             ${getTagsList(item.avatar).map((t) => displayTag(t)).join('')}
                                         </div>
                                     </div>
                                 </div>`;
-    //document.getElementById('desc_zone').value = getDescription(char.id);
+    createTagInput('#input_tag', '#tag_List');
     document.getElementById('desc_zone').value = item.description;
+}
+
+function cleanAndFillList() {
+    charsData = getEntitiesList({ doFilter: false });
+    let charList = charsData.filter(i => i.type === 'character').map((e) => getCharBlock(e.item, e.id)).join('');
+    document.getElementById('character-list').innerHTML = '';
+    document.getElementById('character-list').innerHTML = charList;
 }
 
 function openPopup() {
 
-    if (savedPopupContent) {
-        console.log('Using saved popup content');
-        // Append the saved content to the popup container
-        callPopup('', 'text', '', { okButton: 'Close', wide: true, large: true })
-            .then(() => {
-                savedPopupContent = document.querySelector('.list-character-wrapper');
-            });
-
-        document.getElementById('dialogue_popup_text').appendChild(savedPopupContent);
-        return;
-    }
-
     charsData = getEntitiesList({ doFilter: false });
 
-    const listLayout = popupState ? popupState : `
+    const listLayout = `
     <div class="list-character-wrapper flexFlowColumn" id="list-character-wrapper">
         <div class="character-list" id="character-list">
-            ${charsData.filter(i => i.type === 'character').map((e) => getCharBlock(e.item, e.id)).join('')}
+        ${charsData.filter(i => i.type === 'character').map((e) => getCharBlock(e.item, e.id)).join('')}
         </div>
         <hr>
         <div class="character-details" id="char-details" style="display:none">
@@ -125,10 +120,7 @@ function openPopup() {
     `;
 
     // Call the popup with our list layout
-    callPopup(listLayout, 'text', '', { okButton: 'Close', wide: true, large: true })
-        .then(() => {
-            savedPopupContent = document.querySelector('.list-character-wrapper');
-        });
+    callPopup(listLayout, 'text', '', { okButton: 'Close', wide: true, large: true });
 }
 
 jQuery(async () => {
@@ -141,6 +133,8 @@ jQuery(async () => {
 
     $(document).on('click', '.char_select', async function () {
         const id = $(this).attr('chid');
+        setMenuType('character_edit');
+        setCharacterId(id);
 
         fillDetails(charsData.filter(i => i.id == id && i.type === 'character')[0]);
 
