ca0c12fe948b0d13a138c4e2e80d2acb40b607d9
Rendal <23568313+sakhavhyand@users.noreply.github.com>
2024-04-23 16:16:06 +0200

    Adding duplication and export.

--- src/atm_characters.js
+++ src/atm_characters.js
@@ -1,9 +1,12 @@
-import { getOneCharacter, getRequestHeaders } from '../../../../../script.js';
+import {
+    getCharacters,
+} from '../../../../../script.js';
 
-export { editChar };
+export { editChar, delChar, dupeChar };
 
 const event_types = SillyTavern.getContext().eventTypes;
 const eventSource = SillyTavern.getContext().eventSource;
+const getRequestHeaders = SillyTavern.getContext().getRequestHeaders;
 
 async function editChar(update) {
     const this_chid = SillyTavern.getContext().characterId;
@@ -17,10 +20,47 @@ async function editChar(update) {
     });
 
     if (response.ok) {
-        await getOneCharacter(update.avatar);
+        await getCharacters();
         await eventSource.emit(event_types.CHARACTER_EDITED, { detail: { id: this_chid, character: characters[this_chid] } });
     } else {
         console.log('Error!');
     }
 }
 
+// Function not used at this moment, leaving it here just in case
+async function delChar(avatar, delChats = true) {
+
+    const toDel = {
+        avatar_url: avatar,
+        delete_chats: delChats,
+    };
+
+    const response = await fetch('/api/characters/delete', {
+        method: 'POST',
+        headers: getRequestHeaders(),
+        body: JSON.stringify(toDel),
+        cache: 'no-cache',
+    });
+
+    if (response.ok) {
+        // TO DO ?
+    } else {
+        console.log('Error!');
+    }
+}
+
+async function dupeChar(avatar) {
+    const body = { avatar_url: avatar };
+    const response = await fetch('/api/characters/duplicate', {
+        method: 'POST',
+        headers: getRequestHeaders(),
+        body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+        toastr.success('Character Duplicated');
+        const data = await response.json();
+        await eventSource.emit(event_types.CHARACTER_DUPLICATED, { oldAvatar: body.avatar_url, newAvatar: data.path });
+        await getCharacters();
+    }
+}
